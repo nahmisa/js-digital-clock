6de70f944790ee4b0077ef35237feee82eaade11
Sarah Nahmias <nahmisa@gmail.com>
2016-11-18 14:28:15 -0800

    Displays an updated time every second

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 $(document).ready(function() { //anonymous function
-  $('#clock').append('<p>hello world<p>');
-// keep all your stuff inside of there
+  var getTime = function(){
+    var d = new Date(); // for now
+    return [d.getHours(), d.getMinutes(), d.getSeconds()];
+  };
+
+  var updateTime = function(){
+    // get and format the current time
+    var timeNow = getTime();
+    var timeString = timeNow[0] + ":" + timeNow[1] + "." + timeNow[2];
+    // clear the contents of the div so we replace the time instead
+    // of stringing it together horizontally
+    $('#clock').html("");
+    $('#clock').append(timeString);
+  };
+
+  // call updateTime at an interval of every second -- time is in miliseconds
+  var intervalID = window.setInterval(updateTime, 1000);
+
 });
